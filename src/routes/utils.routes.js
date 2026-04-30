4b26048d6e07c2721f8ca077dbc1fa0c1582f771
Tilakraj <tsr.6261@gmail.com>
2026-04-30 17:30:18 +0530

    feat: implement internal auth middelware for internal routes

--- src/routes/utils.routes.js
+++ src/routes/utils.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import utilsController from "../controllers/utils.controller.js";
 import * as agentConfigController from "../controllers/agentConfig.controller.js";
-import { middleware } from "../middlewares/middleware.js";
+import { middleware, InternalAuth } from "../middlewares/middleware.js";
 import validate from "../middlewares/validate.middleware.js";
 import utilsValidation from "../validation/joi_validation/utils.validation.js";
 import agentConfigValidation from "../validation/joi_validation/agentConfig.validation.js";
@@ -9,8 +9,8 @@ import { setModelStatusAdminBodySchema } from "../validation/joi_validation/mode
 
 const router = express.Router();
 
-router.delete("/redis", validate(utilsValidation.clearRedisCache), utilsController.clearRedisCache);
-router.get("/redis/:id", validate(utilsValidation.getRedisCache), utilsController.getRedisCache);
+router.delete("/redis", middleware, InternalAuth, validate(utilsValidation.clearRedisCache), utilsController.clearRedisCache);
+router.get("/redis/:id", middleware, InternalAuth, validate(utilsValidation.getRedisCache), utilsController.getRedisCache);
 router.post("/call-gtwy", middleware, validate(utilsValidation.callAi), utilsController.callGtwy);
 router.get(
   "/getBridgesAndVersions/:modelName",
@@ -21,6 +21,6 @@ router.post("/token", middleware, validate(utilsValidation.generateToken), utils
 router.post("/affiliate/embed-token", middleware, validate(utilsValidation.getAffiliateEmbedToken), utilsController.getAffiliateEmbedToken);
 router.get("/users-details", middleware, utilsController.getCurrentOrgUsers);
 router.delete("/agent/:agent_id", middleware, validate(agentConfigValidation.getAgent), agentConfigController.permanentlyDeleteAgentController);
-router.patch("/models/status", middleware, validate({ body: setModelStatusAdminBodySchema }), utilsController.setModelStatus);
+router.patch("/models/status", middleware, InternalAuth, validate({ body: setModelStatusAdminBodySchema }), utilsController.setModelStatus);
 
 export default router;
